e0f048c4763def4108f5587e9017f077c55455cf
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-09-26 23:30:51 +0100

    add a NewWaitGroup implementation to create more like an object of WaitGroup

--- waitsync/sync.go
+++ waitsync/sync.go
@@ -9,3 +9,9 @@ type WaitGroup struct {
 	counter int
 	mutex   sync.Mutex
 }
+
+// NewWaitGroup initializes and returns a new WaitGroup
+func NewWaitGroup() *WaitGroup {
+	wg := &WaitGroup{}
+	return wg
+}
