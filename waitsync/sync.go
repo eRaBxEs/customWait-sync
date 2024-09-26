0055036590498c198277fb1f3cff5faf188a272c
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-09-26 23:37:09 +0100

    create the implementation of the add that increments the counter by a given delta

--- waitsync/sync.go
+++ waitsync/sync.go
@@ -15,3 +15,11 @@ func NewWaitGroup() *WaitGroup {
 	wg := &WaitGroup{}
 	return wg
 }
+
+// Now to implement some of the needed methods by a Waitgroup
+// Add increments the waitGroup counter by a specified delta
+func (wg *WaitGroup) Add(delta int) {
+	wg.mutex.Lock()
+	wg.counter += delta
+	wg.mutex.Unlock()
+}
