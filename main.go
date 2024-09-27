cff8926d6291f7f04a6a8f922caacb6ddc4c2f43
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-09-27 23:57:41 +0100

    use the implementation of the customised WaitGroup in main

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"custom-wait/waitsync"
+	"fmt"
+	"time"
 )
 
 func main() {
 	var wg = waitsync.NewWaitGroup() // create an instance of the customised WaitGroup
+
+	// To create on the fly 5 goroutines
+	for i := 1; i < 5; i++ {
+		go func(i int) {
+			wg.Add(1)
+			defer wg.Done()
+			time.Sleep(time.Duration(i))
+			fmt.Printf("Goroutine %d just finished\n", i)
+
+		}(i)
+	}
+
+	fmt.Println("All goroutine successfully finished!")
+
 }
