87fd8e44966136f6416de66636f7bc8fad24decf
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-09-28 14:42:31 +0100

    brought the call to the Add method just before the goRoutines and properly adjusted the duration in seconds

--- main.go
+++ main.go
@@ -10,11 +10,11 @@ func main() {
 	var wg = waitsync.NewWaitGroup() // create an instance of the customised WaitGroup
 
 	// To create on the fly 5 goroutines
-	for i := 1; i < 5; i++ {
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
 		go func(i int) {
-			wg.Add(1)
 			defer wg.Done()
-			time.Sleep(time.Duration(i))
+			time.Sleep(time.Second * time.Duration(i))
 			fmt.Printf("Goroutine %d just finished\n", i)
 
 		}(i)
